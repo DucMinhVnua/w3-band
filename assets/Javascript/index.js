3a0609f32c9f556333e80ab4e9cbf57a3ca3c6eb
unknown <ndminhvnua@gmail.com>
2020-12-25 01:18:28 +0700

    update mobile menu

---
+++ assets/Javascript/index.js
@@ -0,0 +1,23 @@
+// Đóng mở menu
+var header = document.getElementById('header');
+var mobileMenu = document.querySelector('.fa-bars');
+var heightHeader = header.clientHeight;
+
+mobileMenu.onclick = function() {
+    var height = header.clientHeight == heightHeader;
+    if (height) {
+        header.style.height = 'auto';
+    } else {
+        header.style.height = null;
+    }
+}
+
+// Tự động đóng khi chọn menu
+var menuItems = document.querySelectorAll('#nav li a[href*="#"]');
+var navLis = document.querySelectorAll('#nav li');
+for (var i = 0; i < menuItems.length; i++) {
+    var menuItem = menuItems[i];
+    menuItem.onclick = function () {
+        header.style.height = null;
+    }
+}
